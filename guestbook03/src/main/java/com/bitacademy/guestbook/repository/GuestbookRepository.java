10675cbc849911a2d1b344b30947cc260e1fee8b
HJ1510 <yhj221004@gmail.com>
2022-11-15 16:49:15 +0900

    guestbook03

---
+++ guestbook03/src/main/java/com/bitacademy/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,156 @@
+package com.bitacademy.guestbook.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.bitacademy.guestbook.vo.GuestbookVo;
+
+@Repository // @Repository 표시 꼭꼭!!!!!
+public class GuestbookRepository {
+
+	public List<GuestbookVo> findAll() {
+		List<GuestbookVo> result = new ArrayList<>();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			String sql = "select no, name, contents, reg_date  from guestbook order by reg_date desc";
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery(); // rs = pstmt.executeQuery(sql); 오류는 안나지만..문법상 없애는게 좋음
+
+			while (rs.next()) {
+
+				int no = rs.getInt(1);
+				String name = rs.getString(2);
+				String contents = rs.getString(3);
+				String regDate = rs.getString(4);
+
+				GuestbookVo vo = new GuestbookVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setContents(contents);
+				vo.setRegDate(regDate);
+
+				result.add(vo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Error : " + e);
+		} finally {
+			try {
+				if (rs != null) { // 닫는 순서는 생성 역순으로
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	public Boolean insert(GuestbookVo vo) {
+		boolean result = false;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = "insert into guestbook values(null, ?, ?, ?, now())";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getContents());
+			//
+
+			int count = pstmt.executeUpdate(); // int count = pstmt.executeUpdate(sql); 에러!
+
+			result = count == 1;
+
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	public Boolean deleteByNoAndPassword(Long no, String password) {
+		boolean result = false;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = "delete from guestbook where no = ? and password = ?";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setLong(1, no);
+			pstmt.setString(2, password);
+
+			int count = pstmt.executeUpdate();
+
+			result = count == 1;
+
+		} catch (SQLException e) {
+			System.out.println("Error : " + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+
+	}
+
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8"; // 마리아db에선 utf-8로 하면 오류! 뒤에 옵션은 추가 가능
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Class Not Found:" + e);
+		}
+		return conn;
+	}
+}
